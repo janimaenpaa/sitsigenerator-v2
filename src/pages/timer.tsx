369e39321d6699e41f533bb2b63c3dbea01a224e
janimaenpaa <itsmayd@gmail.com>
2021-10-27 17:06:03 +0300

    Add Timer and Settins page

---
+++ src/pages/timer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const Timer = (props: Props) => {
+    return (
+        <div>
+            <h2>15:00</h2>
+        </div>
+    )
+}
+
+export default Timer
