376fa60287c57857d714a0bc0a6c88b726e1093b
janimaenpaa <itsmayd@gmail.com>
2021-11-23 18:29:38 +0200

    Add styles for settings page

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -3,6 +3,14 @@ import React from "react";
 import prisma from "../lib/prisma";
 import { Punishment } from "../types";
 import Link from "next/link";
+import {
+  Button,
+  Card,
+  Container,
+  Text,
+  TextInput,
+  Title,
+} from "@mantine/core";
 
 interface Props {
   punishments: Punishment[];
@@ -11,22 +19,25 @@ interface Props {
 const Settings = ({ punishments }: Props) => {
   console.log({ punishments });
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-      }}
-    >
-      <Link href="/">Takasin</Link>
-      <h2>Settings</h2>
-      <ul>
+    <Container>
+      <Link href="/" passHref>
+        <Button mt={20}>Takasin</Button>
+      </Link>
+      <Container style={{ flexDirection: "column", paddingTop: 10 }}>
+        <Title align="center">Asetukset</Title>
+        <Title order={2}>Rangaistukset</Title>
+        <TextInput label="Lisää rangaistus" />
+        <Button style={{ marginTop: 10, marginBottom: 20 }} fullWidth>
+          Lisää
+        </Button>
         {punishments.map((p) => (
-          <li key={p.id}>{p.description}</li>
+          <Card key={p.id} withBorder style={{ marginTop: 10 }}>
+            <Text>{p.description}</Text>
+            <Button color="red">Poista</Button>
+          </Card>
         ))}
-      </ul>
-    </div>
+      </Container>
+    </Container>
   );
 };
 
