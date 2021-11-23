d09a52d164948f05c540a850b8ac88846b9464ed
janimaenpaa <itsmayd@gmail.com>
2021-11-23 17:37:43 +0200

    Get punishments from db

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,15 +1,38 @@
-import React from 'react'
+import { GetStaticProps } from "next";
+import React from "react";
+import prisma from "../lib/prisma";
+import { Punishment } from "../types";
+import Link from "next/link";
 
 interface Props {
-    
+  punishments: Punishment[];
 }
 
-const Settings = (props: Props) => {
-    return (
-        <div>
-            Settings
-        </div>
-    )
-}
+const Settings = ({ punishments }: Props) => {
+  console.log({ punishments });
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <Link href="/">Takasin</Link>
+      <h2>Settings</h2>
+      <ul>
+        {punishments.map((p) => (
+          <li key={p.id}>{p.description}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
-export default Settings
+export const getStaticProps: GetStaticProps = async () => {
+  const punishments = await prisma.punishment.findMany();
+  return { props: { punishments } };
+};
+
+export default Settings;
