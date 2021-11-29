8715977988bbf0875865761acd71dcea17d13c9d
janimaenpaa <itsmayd@gmail.com>
2021-11-30 00:04:16 +0200

    Add CRUD to punishments

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,23 +1,58 @@
-import { GetStaticProps } from "next";
-import React from "react";
-import prisma from "../lib/prisma";
+import { useEffect, useState } from "react";
 import { Punishment } from "../types";
 import Link from "next/link";
-import {
-  Button,
-  Card,
-  Container,
-  Text,
-  TextInput,
-  Title,
-} from "@mantine/core";
+import { Button, Card, Container, Text, TextInput, Title } from "@mantine/core";
 
-interface Props {
-  punishments: Punishment[];
-}
+const Settings = () => {
+  const [punishment, setPunishment] = useState("");
+  const [punishments, setPunishments] = useState<Punishment[] | null>(null);
 
-const Settings = ({ punishments }: Props) => {
-  console.log({ punishments });
+  const getPunishments = () => {
+    fetch("/api/punishments")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+        setPunishments(data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  const handleAddPunishment = () => {
+    fetch("/api/punishments", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ description: punishment }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+        getPunishments();
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  const handleDelete = (id: number) => {
+    fetch(`/api/punishments/${id}`, { method: "DELETE" })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Deleted");
+        getPunishments();
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+
+  useEffect(() => {
+    getPunishments();
+  }, []);
+
   return (
     <Container>
       <Link href="/" passHref>
@@ -26,24 +61,33 @@ const Settings = ({ punishments }: Props) => {
       <Container style={{ flexDirection: "column", paddingTop: 10 }}>
         <Title align="center">Asetukset</Title>
         <Title order={2}>Rangaistukset</Title>
-        <TextInput label="Lisää rangaistus" />
-        <Button style={{ marginTop: 10, marginBottom: 20 }} fullWidth>
+        <TextInput
+          label="Lisää rangaistus"
+          onChange={(event) => setPunishment(event.target.value)}
+          value={punishment}
+        />
+        <Button
+          fullWidth
+          onClick={handleAddPunishment}
+          style={{ marginTop: 10, marginBottom: 20 }}
+        >
           Lisää
         </Button>
-        {punishments.map((p) => (
-          <Card key={p.id} withBorder style={{ marginTop: 10 }}>
-            <Text>{p.description}</Text>
-            <Button color="red">Poista</Button>
-          </Card>
-        ))}
+        {punishments && punishments.length > 0 ? (
+          punishments.map((p) => (
+            <Card key={p.id} withBorder style={{ marginTop: 10 }}>
+              <Text>{p.description}</Text>
+              <Button color="red" onClick={() => handleDelete(p.id)}>
+                Poista
+              </Button>
+            </Card>
+          ))
+        ) : (
+          <Card withBorder>Ei rangaistuksia</Card>
+        )}
       </Container>
     </Container>
   );
-};
-
-export const getStaticProps: GetStaticProps = async () => {
-  const punishments = await prisma.punishment.findMany();
-  return { props: { punishments } };
 };
 
 export default Settings;
