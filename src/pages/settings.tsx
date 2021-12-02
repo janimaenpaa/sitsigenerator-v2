7562989c48a8a7065126f22019d9204898d14be9
janimaenpaa <itsmayd@gmail.com>
2021-12-02 11:54:32 +0200

    Add loading state to settings

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import { Punishment, Settings as ISettings } from "../types";
 import Link from "next/link";
 import { Button, Card, Container, Text, TextInput, Title } from "@mantine/core";
@@ -10,6 +10,7 @@ const Settings = () => {
   const [seconds, setSeconds] = useState("");
   const [punishments, setPunishments] = useState<Punishment[] | null>(null);
   const [settingsData, setSettingsData] = useState<ISettings | null>(null);
+  const [loading, setLoading] = useState(true);
 
   const getPunishments = () => {
     fetch("/api/punishments")
@@ -46,6 +47,7 @@ const Settings = () => {
       .then((response) => response.json())
       .then((data) => {
         console.log("Success:", data);
+        setPunishment("");
         getPunishments();
       })
       .catch((error) => {
@@ -93,7 +95,9 @@ const Settings = () => {
   };
 
   useEffect(() => {
-    Promise.all([getPunishments(), getSettingsData()]);
+    Promise.all([getPunishments(), getSettingsData()]).then(() =>
+      setLoading(false)
+    );
   }, []);
 
   return (
@@ -101,72 +105,78 @@ const Settings = () => {
       <Link href="/" passHref>
         <Button mt={20}>Takasin</Button>
       </Link>
-      <Container style={{ flexDirection: "column", paddingTop: 10 }}>
-        <Title align="center">Asetukset</Title>
-        <Title order={2}>Rangaistukset</Title>
-        <TextInput
-          label="Lisää rangaistus"
-          onChange={(event) => setPunishment(event.target.value)}
-          value={punishment}
-        />
-        <Button
-          fullWidth
-          onClick={handleAddPunishment}
-          style={{ marginTop: 10, marginBottom: 20 }}
-        >
-          Lisää
-        </Button>
-        {punishments && punishments.length > 0 ? (
-          punishments.map((p) => (
-            <Card key={p.id} withBorder style={{ marginTop: 10 }}>
-              <Text>{p.description}</Text>
-              <Button color="red" onClick={() => handleDelete(p.id)}>
-                Poista
-              </Button>
-            </Card>
-          ))
-        ) : (
-          <Card withBorder>Ei rangaistuksia</Card>
-        )}
-      </Container>
-      <Container style={{ flexDirection: "column", paddingTop: 10 }}>
-        <Title order={2}>Pöytäryhmät</Title>
-        <TextInput
-          label={`Pöytäryhmien määrä ${settingsData?.tables}`}
-          onChange={(event) => setTables(event.target.value)}
-          value={tables}
-        />
-        <Button
-          fullWidth
-          onClick={handleTables}
-          style={{ marginTop: 10, marginBottom: 20 }}
-        >
-          Muuta
-        </Button>
-      </Container>
-      <Container style={{ flexDirection: "column", paddingTop: 10 }}>
-        <Title order={2}>Ajastin</Title>
-        <Text>{`${settingsData?.minutes} minuutia, ${settingsData?.seconds} sekuntia`}</Text>
-        <Container style={{ width: "100%" }}>
-          <TextInput
-            label={`Minuutit`}
-            onChange={(event) => setMinutes(event.target.value)}
-            value={minutes}
-          />
-          <TextInput
-            label={`Sekuntit`}
-            onChange={(event) => setSeconds(event.target.value)}
-            value={seconds}
-          />
-        </Container>
-        <Button
-          fullWidth
-          onClick={handleTimer}
-          style={{ marginTop: 10, marginBottom: 20 }}
-        >
-          Muuta
-        </Button>
-      </Container>
+      <Title align="center">Asetukset</Title>
+      {loading ? (
+        <Container>Loading...</Container>
+      ) : (
+        <Fragment>
+          <Container style={{ flexDirection: "column", paddingTop: 10 }}>
+            <Title order={2}>Rangaistukset</Title>
+            <TextInput
+              label="Lisää rangaistus"
+              onChange={(event) => setPunishment(event.target.value)}
+              value={punishment}
+            />
+            <Button
+              fullWidth
+              onClick={handleAddPunishment}
+              style={{ marginTop: 10, marginBottom: 20 }}
+            >
+              Lisää
+            </Button>
+            {punishments && punishments.length > 0 ? (
+              punishments.map((p) => (
+                <Card key={p.id} withBorder style={{ marginTop: 10 }}>
+                  <Text>{p.description}</Text>
+                  <Button color="red" onClick={() => handleDelete(p.id)}>
+                    Poista
+                  </Button>
+                </Card>
+              ))
+            ) : (
+              <Card withBorder>Ei rangaistuksia</Card>
+            )}
+          </Container>
+          <Container style={{ flexDirection: "column", paddingTop: 10 }}>
+            <Title order={2}>Pöytäryhmät</Title>
+            <TextInput
+              label={`Pöytäryhmien määrä ${settingsData?.tables}`}
+              onChange={(event) => setTables(event.target.value)}
+              value={tables}
+            />
+            <Button
+              fullWidth
+              onClick={handleTables}
+              style={{ marginTop: 10, marginBottom: 20 }}
+            >
+              Muuta
+            </Button>
+          </Container>
+          <Container style={{ flexDirection: "column", paddingTop: 10 }}>
+            <Title order={2}>Ajastin</Title>
+            <Text>{`${settingsData?.minutes} minuutia, ${settingsData?.seconds} sekuntia`}</Text>
+            <Container style={{ width: "100%" }}>
+              <TextInput
+                label={`Minuutit`}
+                onChange={(event) => setMinutes(event.target.value)}
+                value={minutes}
+              />
+              <TextInput
+                label={`Sekuntit`}
+                onChange={(event) => setSeconds(event.target.value)}
+                value={seconds}
+              />
+            </Container>
+            <Button
+              fullWidth
+              onClick={handleTimer}
+              style={{ marginTop: 10, marginBottom: 20 }}
+            >
+              Muuta
+            </Button>
+          </Container>
+        </Fragment>
+      )}
     </Container>
   );
 };
